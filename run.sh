b8dec39f13caf0e93d2be0584058b95fb1e75064
Dyuman Aditya <dyuman2001@gmail.com>
2024-05-17 23:21:46 +0200

    added sbatch file for supercomputer

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------
+#SBATCH -G a100:1              # number of GPUs
+#SBATCH --exclusive            # request the whole node
+####SBATCH --cpus-per-gpu 20
+#SBATCH -t 12:00:00          # time in d-hh:mm:ss
+#SBATCH -o ./slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e ./slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=END,FAIL   # Send an e-mail when a job stops, or fails
+#SBATCH --mail-user=%u@asu.edu # Mail-to address
+#SBATCH --export=NONE          # Purge the job-submitting shell environment
+#-------------------------------------------------------------------------

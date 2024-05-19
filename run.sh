e73329badcb25ead2d1eab3c983dc4c7b40dcf8a
Dyuman Aditya <dyuman2001@gmail.com>
2024-05-19 20:16:43 +0200

    changed sbatch

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@
 #-------------------------------------------------------------------------
 #SBATCH -G a100:1              # number of GPUs
 #SBATCH --cpus-per-gpu=20
-#SBATCH --mem=100G
-#SBATCH -t 1-00:00:00          # time in d-hh:mm:ss
+#SBATCH --mem=50G              # memory in GB
+#SBATCH -t 0-12:00:00          # time in d-hh:mm:ss
 #SBATCH -o ./slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e ./slurm.%j.err # file to save job's STDERR (%j = JobId)
 #SBATCH --mail-type=END,FAIL   # Send an e-mail when a job stops, or fails

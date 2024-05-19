e185099cdda78e0db935baa5e7c1292c0c562022
Dyuman Aditya <dyuman2001@gmail.com>
2024-05-19 20:21:41 +0200

    changed sbatch

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #-------------------------------------------------------------------------
 #SBATCH -G a100:1              # number of GPUs
-#SBATCH --cpus-per-gpu=20
+#SBATCH --cpus-per-gpu 20
 #SBATCH --mem=50G              # memory in GB
 #SBATCH -t 0-12:00:00          # time in d-hh:mm:ss
 #SBATCH -o ./slurm.%j.out # file to save job's STDOUT (%j = JobId)
